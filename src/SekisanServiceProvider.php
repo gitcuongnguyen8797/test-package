c33b40033b01bedee7b516c2ab89b1a3f60cc4d8
Cuong Nguyen <cuongnguyen.work8797@gmail.com>
2022-02-20 12:41:18 +0700

    Init Sekisan Package

---
+++ src/SekisanServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SyncSystem\Sekisan;
+
+use Illuminate\Support\ServiceProvider;
+
+class SekisanServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/routes.php');
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test-package', function() {
+    return 'Packge Successfully';
+});
+
+?>
